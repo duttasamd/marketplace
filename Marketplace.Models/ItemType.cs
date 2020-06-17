c60f06bf3d95ee3f8dda0e64aced11266cdd7b99
nalanda <nc1592universities@gmail.com>
2020-06-18 00:38:59 +0200

    Fixed ItemType

--- Marketplace.Models/ItemType.cs
+++ Marketplace.Models/ItemType.cs
@@ -1,13 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace Marketplace.Models
 {
     public class ItemType
     {
+        [Key]
         public Guid ItemTypeID { get; set; }
 
-        public String ItemTypeName { get; set; }
+        public string ItemTypeName { get; set; }
 
         public ICollection<Item> Items { get; set; }
     }
