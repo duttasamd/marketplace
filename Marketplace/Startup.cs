cc3882c158815fa159e70bf799294920df0cc374
Samrat Dutta <dutta.sam.d@gmail.com>
2020-06-18 00:34:35 +0200

    Ready to Migrate

--- Marketplace/Startup.cs
+++ Marketplace/Startup.cs
@@ -28,7 +28,8 @@ namespace Marketplace
             services.AddControllersWithViews();
             services
                 .AddDbContextPool<ApplicationDbContext>
-                (options => options.UseMySQL(Configuration.GetConnectionString("DefaultConnection")));
+                (options =>
+                options.UseMySQL(Configuration.GetConnectionString("DefaultConnection"), b => b.MigrationsAssembly("Marketplace")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
